71508bd2b315c5341e536506f78ea58e5cfc741c
nano-git <58063825+nano-git@users.noreply.github.com>
2019-11-23 16:37:49 +0900

    Add and Update
    ボタンを実装

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.piano;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+//必要なものをインポート
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity<ChangeTextView> extends AppCompatActivity {
+
+    private TextView textView;
+    private boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ボタン指定
+        Button button = findViewById(R.id.ChangeButton);
+        //テキストビュー指定
+        textView = findViewById(R.id.messageTextView);
+
+        //ボタン押下時の動作指定
+        button.setOnClickListener(new View.OnClickListener() {
+
+            //ボタンが押されたらonClickメソッドが起動する
+            public void onClick(View view){
+                if(flag) {
+                    //flagがtrueのとき「Hello World」をテキストビューに表示
+                    textView.setText("Hello World");
+                    flag=false;
+                } else {
+                    //flagがfalseのとき「android」をテキストビューに表示
+                    textView.setText("android");
+                    flag=true;
+                }
+            }
+        });
     }
+
+
 }
